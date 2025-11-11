09ef7c9f1771ec6eb209a0d52fb2b9abfb0b03c9
BubbleDude <minusschwam@gmail.com>
2025-11-11 20:19:45 +0100

    Update url_launcher.dart

--- lib/utils/url_launcher.dart
+++ lib/utils/url_launcher.dart
@@ -2,8 +2,9 @@ import 'dart:developer';
 
 import 'package:url_launcher/url_launcher.dart';
 
-Future<void> launch_Url(_url) async {
-  if (!await launchUrl(_url, mode: LaunchMode.externalApplication)) {
-    log('Could not launch $_url', name: "launch_Url");
+Future<void> launch_Url(dynamic url) async {
+  final uri = url is Uri ? url : Uri.parse(url.toString());
+  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+    log('Could not launch $uri', name: "launch_Url");
   }
 }
